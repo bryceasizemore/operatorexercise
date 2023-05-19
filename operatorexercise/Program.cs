9a7815d5f5107b93d6de3982260530cc484549e4
bryce sizemore <brxccce@outlook.com>
2023-05-18 23:55:46 -0700

    opex fin

--- operatorexercise/Program.cs
+++ operatorexercise/Program.cs
@@ -1,10 +1,34 @@
 ﻿using System;
-using System.Security.Cryptography.X509Certificates;
 
 namespace OperatorExercise
-{ 
+{
     class Program
-    { 
+    {
         static void Main(string[] args)
-        { 
+        {
             //For divison, create two integer variables
+
+            int a = 17;
+            int b = 4;
+            var quotient = a / b;
+            var remainder =a % b;
+
+            Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+
+            Console.WriteLine("please enter the radius of your circle:");
+            var userInput = Console.ReadLine();
+            var radius = double.Parse(userInput);
+
+            AreaOfCircle(radius);
+            AreaOfCircle(15);
+        }
+
+        //define a method - which performs some functionality. that can be used over and over again
+        public static double AreaOfCircle(double radius) 
+        { 
+            var area = Math.PI * Math.Pow(radius, 2);
+            
+            return area; 
+        }
+    }
+}

51d0ed89e8e410d8d31b95bb7ae661968a8fd024
bryce sizemore <brxccce@outlook.com>
2023-05-18 22:56:51 -0700

    Add project files.

---
+++ operatorexercise/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace OperatorExercise
+{ 
+    class Program
+    { 
+        static void Main(string[] args)
+        { 
+            //For divison, create two integer variables
